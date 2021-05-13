2bd4f81f6e7287e3291d00094742d821f73e9199
LsPartida <beluis1993@gmail.com>
2021-05-13 08:22:56 -0500

    * AddUser component created ** Card component created ** Button component created

---
+++ users/src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import AddUser from "./components/AddUser";
+function App() {
+  return (
+    <div className="App">
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
