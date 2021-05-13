c2a6c8aa0fa4899c49b19daf10e825f253303b29
Luis Partida <node.support3@swdozar.com>
2021-05-13 16:37:41 -0500

    State management finished

--- users/src/App.js
+++ users/src/App.js
@@ -1,9 +1,22 @@
-import logo from "./logo.svg";
-import AddUser from "./components/AddUser";
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div className="App">
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
